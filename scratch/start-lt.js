6344966073662c6cfd920802dc84cc1e53346fc7
kaefha <yourefha@gmail.com>
2026-08-18 17:16:40 +0700

    chore: optimize cross-platform scripts for Windows & macOS compatibility and remove non-essential temporary files

--- scratch/start-lt.js
+++
@@ -1,7 +0,0 @@
-const localtunnel = require('localtunnel');
-const fs = require('fs');
-(async () => {
-  const tunnel = await localtunnel({ port: 3000 });
-  fs.writeFileSync('lt-url.txt', tunnel.url);
-  console.log('Tunnel started at:', tunnel.url);
-})();
